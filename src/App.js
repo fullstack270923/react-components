ab4e910963429df97464004c5266176b4866a1f9
fullstack290623 <fullstack290623@gmail.com>
2024-04-17 17:46:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    // data
+    state = {
+      name: 'Danny',
+      age: 30
+    }
+
+    // redner
+    render() {
+      //let x 
+      //this.x // error
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>Hello {this.state.name}</h1>
+            <h1>AGE = {this.state.age}</h1>
+
+          </header>
+        </div>
+      );
+    }
 }
 
 export default App;
