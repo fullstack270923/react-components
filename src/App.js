053af3fcc0408972a70d44f375ade48d655d6cce
fullstack290623 <fullstack290623@gmail.com>
2024-04-17 19:22:34 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,53 +1,27 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
 
-class App extends React.Component {
+const StyledButton = styled.button`
+  background-color: ${props => props.primary ? 'blue' : 'gray'};
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 5px;
 
-  // data
-  // useState
-  state = {
-    time: new Date(),
-    username: 'Danny',
-    email: 'incognito@johndoe.com',
-    age: 30
+  &:hover {
+    background-color: ${props => props.primary ? 'darkblue' : 'darkgray'};
   }
+`;
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
-    // this.setState({ username: e.target.value })
-    // this.setState({ age: e.target.value })
-  }
-
-  // useEffect
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      this.setState({ time: new Date() })
-    }, 1000)
-  }
-
-
-  // redner
-  render() {
-    //let x 
-    //this.x // error
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Hello {this.state.username}</h1>
-          <h1>AGE = {this.state.age}</h1>
-          <h1>TIME = {
-            (this.state.time.getHours() > 9 ? this.state.time.getHours() : ('0' + this.state.time.getHours())) + ':'+
-            (this.state.time.getMinutes() > 9 ? this.state.time.getMinutes() : ('0' + this.state.time.getMinutes())) + ':'+
-            (this.state.time.getSeconds() > 9 ? this.state.time.getSeconds() : ('0' + this.state.time.getSeconds()))}</h1>
-          <button onClick={() => this.setState({ age: this.state.age + 1 })}>ADD AGE</button>
-          <button onClick={() => this.setState({ age: this.state.age - 1 })}>SUB AGE</button>
-          <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
-          <input type="number" name="age" value={this.state.age} onChange={this.handleChange} />
-          <input type="number" name="age"  />
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <StyledButton primary>Primary Button</StyledButton>
+      <StyledButton>Secondary Button</StyledButton>
+    </div>
+  );
 }
 
 export default App;
